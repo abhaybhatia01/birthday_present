39a89dcbb22194d978eebe9d42b2446ad1c46a1a
Abhay Bhatia <abhay1732002@gmail.com>
2022-07-12 17:11:11 +0530

    speech

--- app.js
+++ app.js
@@ -124,3 +124,32 @@ document.querySelector('.video-button4').addEventListener('click',
     videoE4.play();
   }
 })
+
+
+
+
+
+
+
+var animateButton = function(e) {
+
+  e.preventDefault;
+  //reset animation
+  e.target.classList.remove('animate');
+  
+  e.target.classList.add('animate');
+  setTimeout(function(){
+    e.target.classList.remove('animate');
+  },700);
+};
+
+var bubblyButtons = document.getElementsByClassName("bubbly-button");
+
+for (var i = 0; i < bubblyButtons.length; i++) {
+  bubblyButtons[i].addEventListener('click', animateButton, false);
+}
+
+
+
+
+
