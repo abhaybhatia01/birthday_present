0f3df96ff5473cfe49599ba061acc0d9283676c8
Abhay Bhatia <abhay1732002@gmail.com>
2022-07-14 11:30:19 +0530

    final speech changes

--- app.js
+++ app.js
@@ -12,7 +12,7 @@
         mm = String(today.getMonth() + 1).padStart(2, "0"),
         yyyy = today.getFullYear(),
         nextYear = yyyy + 1,
-        dayMonth = "07/13/",
+        dayMonth = "07/14/",
         birthday = dayMonth + yyyy;
 
     
